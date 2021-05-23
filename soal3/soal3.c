83ff393c8e0d723d57e9a604abc853f4e6acb885
axelbrians <axelbrians@gmail.com>
2021-05-23 09:56:43 +0700

    fix soal3c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -19,6 +19,7 @@ char base_path[200];
 char file_col[30000][500];
 int counter = 2;
 bool success = false;
+int index_unkown = 2;
 
 const int MODE_FILE = 1;
 const int MODE_DIR = 2;
@@ -33,6 +34,7 @@ void *move_file();
 
 // execv function
 bool is_file_exist();
+bool is_file_type_unknown(void *param);
 int is_directory();
 void create_dir();
 void list_file();
@@ -60,7 +62,7 @@ int main(int argc, char *argv[]) {
 }
 
 void option_wd() {
-    char *argv[] = { "value", "another one", base_path, NULL };
+    char *argv[] = { "dummy", "mode", base_path, NULL };
     option_dir(argv, MODE_WD);
 }
 
@@ -72,17 +74,19 @@ void option_dir(char *argv[], int option) {
     //printf("=== list of file ===\n");
     for (int i = 2; i < counter + 2; i++) {
         param[i] = &file_col[i];
-        //printf("%s\n", param[i]);
+        printf("%s\n", param[i]);
     }
 
     option_file(counter, param, option);
 }
 
 void list_file(char *param_path) {
-    char path[300], path_to_file[300], temp[300];
+    char path[300], path_to_file[300], temp[300], swap[300];
 
     struct dirent *dp;
     DIR *dir = opendir(param_path);
+
+    bool is_unknown = false;
 
     if (!dir) return;
 
@@ -94,9 +98,17 @@ void list_file(char *param_path) {
             strcat(path, "/");
             strcat(path, dp->d_name);
             // printf("%s\n", path);
+            is_unknown = is_file_type_unknown(path);
 
+            if (is_unknown) {
+                strcpy(temp, file_col[index_unkown]);
+                strcpy(file_col[index_unkown], path);
+                strcpy(file_col[counter], temp);
+                index_unkown++;
+            } else {
+                strcpy(file_col[counter], path);
+            }
             
-            strcpy(file_col[counter], path);
             counter++;
 
             list_file(path);
@@ -109,8 +121,9 @@ void list_file(char *param_path) {
 
 void option_file(int argc, char *argv[], int mode) {
     mythread_t mythread;
-    int err, file_stat;
+    int err, file_stat, counter = 0;
     bool is_exist;
+    pthread_t thread[1000];
 
     mythread.isUsed = false;
 
@@ -118,12 +131,13 @@ void option_file(int argc, char *argv[], int mode) {
         is_exist = is_file_exist(argv[i]);
         file_stat = is_directory(argv[i]);
 
-        if (is_exist && file_stat == 0) { 
+        if (is_exist && file_stat == 0) {
+            char *param = (char*) argv[i];
             err = pthread_create(
-                    &mythread.thread, 
+                    &thread[i - 2], 
                     NULL,
                     &move_file,
-                    (void *) argv[i]);
+                    (void *) param);
             mythread.isUsed = true;
             if (mode == MODE_FILE) 
                 printf("File %d : Berhasil Dikategorikan\n", i - 1);
@@ -134,12 +148,21 @@ void option_file(int argc, char *argv[], int mode) {
                 printf("File %d : Sad, gagal :(\n", (i - 1));
         }
 
-        // if (err != 0) printf("\n can't create thread : [%s]\n", strerror(err));
-        // else printf("\n create thread success\n");
+        counter++;
+
+        if (err != 0)
+            printf("\n can't create thread : [%s]\n", strerror(err));
+        else
+            printf("\n create thread success %d\n", i - 2);
     }
     
-    if (mythread.isUsed)
-        pthread_join(mythread.thread, NULL);
+    if (mythread.isUsed){
+        for (int i = 0; i < counter; i++) {
+            if (pthread_join(thread[i], NULL)) {
+                fprintf(stderr, "error: Cannot join thread # %d\n", i);
+            }
+        }
+    }
 
     if (!success && mode == MODE_DIR) {
         printf("Yah, gagal disimpan :(\n");
@@ -190,13 +213,44 @@ void *move_file(void *param) {
 
     // print information about the file that will be moved
     //printf("=== file information ===\n");
-    //printf("path_to_file: %s\nfile_name: %s\nfile_ext: %s\nnew path: %s\n\n", path_to_file, file_name, file_ext, temp);
+    // printf("path_to_file: %s\nfile_name: %s\nfile_ext: %s\nnew path: %s\n\n", path_to_file, file_name, file_ext, temp);
 
     // create dir based on extension
     create_dir(file_ext);
 
     // move file
     rename(path_to_file, temp);
+}
+
+// check is unknown type file or not
+bool is_file_type_unknown(void *param) {
+    char path_to_file[300], file_name[300], file_ext[30], temp[300];
+    char *buffer;
+
+    strcpy(temp, param);
+    buffer = strtok(temp, "/");
+
+    while (buffer != NULL) {
+        // printf("%s ", buffer);
+        if (buffer != NULL) strcpy(file_name, buffer);
+        buffer = strtok(NULL, "/");
+    }
+    // printf("\n");
+    strcpy(temp, file_name);
+    strcpy(path_to_file, param);
+
+    buffer = strtok(temp, ".");
+    buffer = strtok(NULL, "");
+
+
+    
+    if (file_name[0] == '.') { // case for hidden file start with .
+        return false;
+    } else if (buffer != NULL) { // for normal case
+        return false;
+    } else {
+        return true;
+    }
 }
 
 // convert to lowercase
@@ -236,5 +290,8 @@ void create_dir(void *param) {
     strcat(path, value);
     // printf("=== create_dir ===\n path: %s\n\n", path);
     
-    mkdir(path, 0755);
+    int res = mkdir(path, 0755);
+    if (res == -1) {
+        fprintf(stderr, "error: Cannot create dir # %s\n", path);
+    }
 }
